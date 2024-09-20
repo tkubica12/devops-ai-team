059deb781830742649ee0d3743312c74535d5b74
Tomas Kubica <tkubica12@gmail.com>
2024-09-20 12:02:49 +0200

    ui build

---
+++ terraform/capp.ui.tf
@@ -0,0 +1,47 @@
+resource "azurerm_container_app" "ui" {
+  name                         = "ca-ui"
+  container_app_environment_id = azurerm_container_app_environment.main.id
+  resource_group_name          = azurerm_resource_group.main.name
+  revision_mode                = "Single"
+
+  template {
+    min_replicas = 1
+
+    container {
+      name   = "myapp"
+      image  = "ghcr.io/tkubica12/devops-ai-team/ui:latest"
+      cpu    = 0.5
+      memory = "1Gi"
+
+      env {
+        name  = "COSMOS_ENDPOINT"
+        value = azurerm_cosmosdb_account.main.endpoint
+      }
+
+      env {
+        name  = "COSMOS_DATABASE_NAME"
+        value = azurerm_cosmosdb_sql_database.main.name
+      }
+
+      env {
+        name  = "AZURE_CLIENT_ID"
+        value = azurerm_user_assigned_identity.main.client_id
+      }
+    }
+  }
+
+  ingress {
+    target_port      = 8000
+    external_enabled = true
+
+    traffic_weight {
+      latest_revision = true
+      percentage      = 100
+    }
+  }
+
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.main.id]
+  }
+}
