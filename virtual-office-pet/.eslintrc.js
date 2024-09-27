cb500f26b82d34497f16c8ad12ad2359c92e816f
Tomáš Kubica <tkubica12@gmail.com>
2024-09-27 00:43:15 +0000

    agent_coder added files

---
+++ virtual-office-pet/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    'react-app',
+    'react-app/jest'
+  ],
+  rules: {
+    'react/prop-types': 'warn', // Enable prop-types checking
+    // Add more rules as needed
+  }
+};
