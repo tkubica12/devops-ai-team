d0a4d62a3462c45e514f806e5d6a666d702482ed
Tomáš Kubica <tkubica12@gmail.com>
2024-09-27 01:28:18 +0000

    agent_coder added files

--- virtual-office-pet/tailwind.config.js
+++ virtual-office-pet/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.js',
-    './src/**/*.jsx',
-    './public/index.html'
+    './src/**/*.{js,jsx,ts,tsx}',
+    './public/index.html',
   ],
   theme: {
     extend: {},
