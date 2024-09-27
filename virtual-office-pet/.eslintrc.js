3157df18a64c5182ded89bbf74e381f8bcad61b5
Tomáš Kubica <tkubica12@gmail.com>
2024-09-27 02:07:33 +0000

    agent_coder added files

--- virtual-office-pet/.eslintrc.js
+++ virtual-office-pet/.eslintrc.js
@@ -13,6 +13,8 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
   },
   plugins: ['@typescript-eslint'],
 };
