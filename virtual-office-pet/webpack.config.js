ef5b8fad6a0e10b68b0037b20366390a8ff7e099
Tomáš Kubica <tkubica12@gmail.com>
2024-09-28 21:14:45 +0200

    agent_coder added files

---
+++ virtual-office-pet/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    fallback: {
+      "util": require.resolve("util/")
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      }
+    ],
+  },
+};
