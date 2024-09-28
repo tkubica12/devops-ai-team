e953fc6c22184962aae8ff238545a80e75efa4fb
Tomáš Kubica <tkubica12@gmail.com>
2024-09-28 22:27:58 +0200

    agent_coder added files

---
+++ virtual-office-pet/src/components/PremiumFeatures.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const PremiumFeatures = () => (
+  <div className="premium-container">
+    <h2>Premium Features</h2>
+    <ul>
+      <li>Exclusive Pet Travel Adventures</li>
+      <li>Virtual Pet Clubs</li>
+      <li>Advanced Interactions</li>
+      <li>VIP Pets</li>
+    </ul>
+  </div>
+);
+
+export default PremiumFeatures;
