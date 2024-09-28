5d790e725789139c72ad2b33fe82d9e5ad80ecdc
Tomáš Kubica <tkubica12@gmail.com>
2024-09-28 22:19:36 +0200

    agent_coder added files

--- virtual-office-pet/src/components/ui/Button.js
+++ virtual-office-pet/src/components/ui/Button.js
@@ -1,4 +1,4 @@
-export const Button = ({ children, onClick, className }) => (
+export const Button = ({ children, onClick, className = '' }) => (
   <button
     onClick={onClick}
     className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${className}`}
